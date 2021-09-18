d60274fd176bf7575646b28f2796260c5d4197fe
Maxtrue <zhb384708358@163.com>
2021-09-18 09:24:27 +0800

    sync

--- src/LeetCode/Main.java
+++ src/LeetCode/Main.java
@@ -3,30 +3,70 @@ package LeetCode;
 
 import java.util.*;
 
-public class Main {
-    int g = 0;
-
+public class Main{
     public static void main(String[] args) {
         Solution solution = new Solution();
-//        TestThread testThread = new TestThread();
-//        testThread.test();
-
-
-        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
-
-        int[] nums = {29,91,62,76,51};
-        int[][] graph = {{1,3,7,3,1,1,5,4,10,9,3},{1,10,0,9,2,6,10,2,9,6,2},{5,1,8,4,4,1,8,5,5,7,6},{8,2,8,4,3,5,3,1,5,3,4},{5,2,5,8,9,3,8,2,3,9,6},{8,10,4,4,7,7,8,3,8,8,2},{2,10,10,2,7,6,1,8,4,2,4},{8,5,5,4,4,7,5,0,2,7,4},{7,0,9,5,6,4,5,4,7,5,2},{5,0,8,0,2,3,1,0,9,1,0},{2,6,5,3,5,8,1,4,8,0,9}};
-
-        System.out.println(solution.wordBreak("abcdefgh",new String[]{"abc","a","aa","de","df"}));
-
-
+//        System.out.println(solution.strStr("mississippi","issip"));
+        System.out.println(solution.findWords(new char[][]{{'a'}},new String[]{"a"}));
+		// testg git
+        return;
     }
 }
 
 
-//["MaxQueue","max_value","pop_front","max_value","push_back","max_value","pop_front",
-//        "max_value","pop_front","push_back","pop_front","pop_front","pop_front",
-//        "push_back","pop_front","max_value",
-//        [],[],[],[],[46],[],
-//        [],[],[],[868],[],[],[],
-//        [525],[],[]
+
+
+
+
+//// 本题为考试单行多行输入输出规范示例，无需提交，不计分。
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        while (in.hasNextInt()) {// 注意，如果输入是多个测试用例，请通过while循环处理多个测试用例
+//            int a = in.nextInt();
+//            int b = in.nextInt();
+//            System.out.println(a + b);
+//        }
+//    }
+//}
+
+//// 本题为考试多行输入输出规范示例，无需提交，不计分。
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int ans = 0, x;
+//        for(int i = 0; i < n; i++){
+//            for(int j = 0; j < n; j++){
+//                x = sc.nextInt();
+//                ans += x;
+//            }
+//        }
+//        System.out.println(ans);
+//    }
+//}
+
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();sc.nextLine();
+//        String[] proxy = new String[n];
+//        for (int i = 0; i < n; i++) {
+//            proxy[i]=sc.nextLine();
+////            System.out.println(proxy[i]);
+//        }
+//        int m = sc.nextInt();sc.nextLine();
+//        String[] server = new String[m];
+//        for (int i = 0; i < m; i++) {
+//            server[i]=sc.nextLine();
+//
+////            System.out.println(server[i]);
+//        }
+//    }
+//}
+
+
--- src/LeetCode/Solution.java
+++ src/LeetCode/Solution.java
@@ -1,16 +1,21 @@
 package LeetCode;
 
 
-
-
-import sun.nio.cs.ArrayEncoder;
-
 import java.util.*;
 
 class Solution {
-    public boolean wordBreak(String s, List<String> wordDict) {
-        int ac;
-        int g;
+    public boolean isStraight(int[] nums) {
+        int low=13,high=0;
+        Arrays.sort(nums);
+        int i=0;
+        while(i<5&&nums[i]==0) i++;
+        if(i==5) return true;
+        if(nums[4]-nums[i]>4) return false;
+        while (i<4){
+            if(nums[i]==nums[i+1]) return false;
+            i++;
+        }
+        return true;
 
     }
 }
